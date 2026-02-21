64277d8245ad32b6ab3d57e8e0675f2e5421fc79
Parek4Life <125895505+Cherttov@users.noreply.github.com>
2026-02-21 20:02:14 +0100

    Updated names

--- addons/dev-console/plugin.gd
+++ addons/dev-console/plugin.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 const AUTOLOAD_NAME: String = "DevConsole";
-const AUTOLOAD_PATH: String = "res://addons/console/console.tscn";
+const AUTOLOAD_PATH: String = "res://addons/dev-console/dev-console.tscn";
 
 func _enter_tree() -> void:
 	add_autoload_singleton(AUTOLOAD_NAME, AUTOLOAD_PATH);
