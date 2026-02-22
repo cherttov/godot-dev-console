f139c9ce4656cecb63de15c2854f64ad7cb597c0
Parek4Life <125895505+Cherttov@users.noreply.github.com>
2026-02-22 11:29:10 +0100

    Added input history

--- addons/dev-console/dev-console.gd
+++ addons/dev-console/dev-console.gd
@@ -2,6 +2,8 @@ extends CanvasLayer
 
 # Commands dictionary
 var commands: Dictionary[String, Callable];
+var command_history: Array[String];
+var history_index: int = -1;
 
 # Console root
 @onready var console_viewport: Control = $Control;
@@ -14,6 +16,7 @@ var drag_offset: Vector2 = Vector2.ZERO;
 @export var console_title_label: String = "CONSOLE";
 @export var console_toggle_keybind: Key = KEY_QUOTELEFT;
 @export var console_use_default_commands: bool = true;
+@export var console_use_history: bool = true;
 
 # --------- Init ---------
 func _ready() -> void:
@@ -25,6 +28,7 @@ func _ready() -> void:
 	%Input.release_focus();
 	%Input.clear();
 	commands = {};
+	command_history = [];
 	
 	# Load default commands
 	if console_use_default_commands:
@@ -41,8 +45,10 @@ func _ready() -> void:
 
 # --------- Input ---------
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("dev_console_toggle") and !%Input.has_focus():
+	# Toggle console
+	if event.is_action_pressed("dev_console_toggle"):
 		visible = !visible;
+		console_viewport.position = Vector2(0.0, 0.0);
 		
 		if visible:
 			%Input.grab_focus();
@@ -51,6 +57,13 @@ func _input(event: InputEvent) -> void:
 			%Input.release_focus();
 		
 		get_viewport().set_input_as_handled();
+	
+	# Arrow up/down (history)
+	if visible and console_use_history and !command_history.is_empty():
+		if event.is_action_pressed("dev_console_arrow_up"):
+			_navigate_history(1);
+		elif event.is_action_pressed("dev_console_arrow_down"):
+			_navigate_history(-1);
 
 # --------- Command adding ---------
 func add_command(command_name: String, callback: Callable) -> void:
@@ -63,12 +76,20 @@ func _on_input_submitted(input: String) -> void:
 		%Input.clear();
 		return;
 	
+	# Command history
+	if command_history.is_empty() or command_history.back() != clean_input:
+		command_history.append(clean_input);
+	history_index = -1
+	
+	# Splitting input
 	var parts: PackedStringArray = clean_input.split(" ");
 	var command_name: String = parts[0].strip_edges();
 	parts.remove_at(0);
 	
+	# Outputting input
 	_output_input(clean_input);
 	
+	# Calling callback & outputting result
 	if commands.has(command_name):
 		var result: Variant = commands[command_name].callv(parts);
 		
@@ -127,6 +148,21 @@ func _on_panel_gui_input(event: InputEvent) -> void:
 	elif event is InputEventMouseMotion and dragging:
 		console_viewport.position = console_viewport.get_global_mouse_position() - drag_offset;
 
+# --------- Command history ---------
+func _navigate_history(direction: int) -> void:
+	history_index += direction;
+	
+	if history_index >= command_history.size():
+		history_index = -1;
+		%Input.clear();
+	elif history_index < 0:
+		history_index = 0;
+	
+	if history_index != -1:
+		var command = command_history[(command_history.size() - 1) - history_index];
+		%Input.text = command
+		%Input.caret_column = command.length();
+
 # --------- Keybinds mapping ---------
 func _ensure_keybinds() -> void:
 	if !InputMap.has_action("dev_console_toggle"):
@@ -135,3 +171,20 @@ func _ensure_keybinds() -> void:
 		var event_key: InputEventKey = InputEventKey.new();
 		event_key.physical_keycode = console_toggle_keybind;
 		InputMap.action_add_event("dev_console_toggle", event_key);
+	
+	if console_use_history:
+		# Arrow up
+		if !InputMap.has_action("dev_console_arrow_up"):
+			InputMap.add_action("dev_console_arrow_up");
+			
+			var event_key: InputEventKey = InputEventKey.new();
+			event_key.physical_keycode = KEY_UP;
+			InputMap.action_add_event("dev_console_arrow_up", event_key);
+		
+		# Arrow down
+		if !InputMap.has_action("dev_console_arrow_down"):
+			InputMap.add_action("dev_console_arrow_down");
+			
+			var event_key: InputEventKey = InputEventKey.new();
+			event_key.physical_keycode = KEY_DOWN;
+			InputMap.action_add_event("dev_console_arrow_down", event_key);
